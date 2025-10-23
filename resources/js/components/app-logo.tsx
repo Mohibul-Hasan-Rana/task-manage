711b3e801d10f9ceff7da3d0625da073dba513f8
mohibul <rana.csesust@gmail.com>
2025-10-23 14:00:58 +0600

    changed logo

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -3,12 +3,12 @@ import AppLogoIcon from './app-logo-icon';
 export default function AppLogo() {
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+            <div className="flex aspect-square w-8 h-8 items-center justify-center rounded-md bg-white text-blue-600">
+                <AppLogoIcon width={20} height={20} stroke="currentColor" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-tight font-semibold">
-                    Laravel Starter Kit
+                    TaskMinder
                 </span>
             </div>
         </>
